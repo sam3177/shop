9b4b8293b49a5860e724c14a76ff5aa9be822cf6
sam <silviu.iulian734@gmail.com>
2021-02-14 19:15:01 +0100

    final touches to go

--- routes/cart.js
+++ routes/cart.js
@@ -10,30 +10,30 @@ router.post(
 	async (req, res) => {
 		const product = await productsRepo.getOne(
 			req.params.id
-      );
-      //if cart already exists
+		);
+		//if cart already exists
 		if (req.session.cartId) {
 			const cart = await cartsRepo.getOne(
 				req.session.cartId
 			);
 			const existing = cart.products.find(
 				(p) => p.id === product.id
-         );
-         //if existing product, increment it's 'count' val
+			);
+			//if existing product, increment it's 'count' val
 			if (existing) {
 				existing.count++;
-         }
-         //if not, add it
+			}
 			else {
+				//if not, add it
 				cart.products.push({
 					id    : product.id,
 					count : 1
 				});
 			}
 			await cartsRepo.edit(cart.id, cart);
-      }
-      //if !cart, make one and add product to it
+		}
 		else {
+			//if !cart, make one and add product to it
 			const newCart = {
 				products : []
 			};
@@ -49,16 +49,106 @@ router.post(
 );
 
 //show cart GET
-router.get('/cart', async (req, res)=>{
-   if(req.session.cartId){
-      const cart = await cartsRepo.getOne(req.session.cartId)
-      const products =  cart.products.map(async p=>{
-         const prod = await productsRepo.getOne(p.id)
-         return prod
-      })
-      console.log(products)
-      return res.render('./cart',{products})
-   }
-})
+router.get('/cart', async (req, res) => {
+	if (req.session.cartId) {
+		const cart = await cartsRepo.getOne(
+			req.session.cartId
+		);
+		const products = await Promise.all(
+			cart.products.map(async (p) => {
+				const prod = await productsRepo.getOne(
+					p.id
+				);
+				return { ...prod, count: p.count };
+			})
+		);
+		const total = products.reduce(
+			(prev, prod) =>
+				prev + prod.price * prod.count, 0
+		);
+		return res.render('./cart', {
+			products, total
+		});
+	}
+	else {
+		res.redirect('/');
+	}
+});
+
+//decrement count POST
+router.post(
+	'/cart/products/:id/minus',
+	async (req, res) => {
+		if (req.session.cartId) {
+			const cart = await cartsRepo.getOne(
+				req.session.cartId
+			);
+			const newCartItems = cart.products
+				.map((p) => {
+					if (p.id === req.params.id) {
+						return { ...p, count: p.count - 1 };
+					}
+					else return p;
+				})
+				.filter((p) => p.count >= 1);
+			await cartsRepo.edit(req.session.cartId, {
+				products : newCartItems
+			});
+			return res.redirect('/cart');
+		}
+		else {
+			res.redirect('/');
+		}
+	}
+);
+
+//increment count POST
+router.post(
+	'/cart/products/:id/plus',
+	async (req, res) => {
+		if (req.session.cartId) {
+			const cart = await cartsRepo.getOne(
+				req.session.cartId
+			);
+			const newCartItems = cart.products.map(
+				(p) => {
+					if (p.id === req.params.id) {
+						return { ...p, count: p.count + 1 };
+					}
+					else return p;
+				}
+			);
+			await cartsRepo.edit(req.session.cartId, {
+				products : newCartItems
+			});
+			return res.redirect('/cart');
+		}
+		else {
+			res.redirect('/');
+		}
+	}
+);
+
+//remove product from cart POST
+router.post(
+	'/cart/products/:id/remove',
+	async (req, res) => {
+		if (req.session.cartId) {
+			const cart = await cartsRepo.getOne(
+				req.session.cartId
+			);
+			const newCartItems = cart.products.filter(
+				(p) => p.id !== req.params.id
+			);
+			await cartsRepo.edit(req.session.cartId, {
+				products : newCartItems
+			});
+			return res.redirect('/cart');
+		}
+		else {
+			res.redirect('/');
+		}
+	}
+);
 
 module.exports = router;
